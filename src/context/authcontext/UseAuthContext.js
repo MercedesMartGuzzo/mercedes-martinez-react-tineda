2b0f7b143f576942ddd23ab88895cb6597646488
Mercedes Martinez <mercedesviolin@gmail.com>
2025-12-01 16:11:47 -0300

    fix casing of authcontext folder

--- src/context/authcontext/UseAuthContext.js
+++ src/context/authcontext/UseAuthContext.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { AuthContext } from "./AuthContext"
+import { AuthContext } from "./authcontex/AuthContext"
 
 export const useAuthContext = () => {
     return useContext(AuthContext);
